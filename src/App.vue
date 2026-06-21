e89a13e5a401d226593819e2dc6100d5b7c60923
Kevin Andersson <kevin200032@live.se>
2026-06-21 20:25:45 +0200

    start with header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { supabase } from "../utils/supabase";
+import { BriefcaseBusinessIcon, type LucideProps } from "@lucide/vue";
+import { BriefcaseIcon } from "@lucide/vue";
 
 type jobtype = {
   id: number;
@@ -26,10 +28,19 @@ getJobs();
 
 <template>
   <header>
+    <div class="p-3 gap-3 flex items-center">
+      <div class="bg-black p-1.5 w-fit rounded-xl">
+        <BriefcaseBusinessIcon :size="20" class="text-white" />
+      </div>
+      <div class="flex items-center flex-col">
+        <h1 class="text-sm font-medium">Jobb Sökaren!</h1>
+        <h2 class="text-sm font-mono">{{ Jobs.length }} ansökningar</h2>
+      </div>
+    </div>
+
     <h1 class="text-red-500">Hello World</h1>
-    <p class="text-blue-500" v-for="job in Jobs" :key="job.id">
+    <!-- <p class="text-blue-500" v-for="job in Jobs" :key="job.id">
       {{ job.title }}
-    </p>
-    <p v-for="job in Jobs" :key="job.id">{{ job.company }}</p>
+    </p> -->
   </header>
 </template>
